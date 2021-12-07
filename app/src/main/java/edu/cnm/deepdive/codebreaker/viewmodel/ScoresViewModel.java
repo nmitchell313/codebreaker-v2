7320b532f699e1affaccc8a7aa65f56c20411bcb
nmitchell313 <nmitchell11@cnm.edu>
2021-12-07 11:06:58 -0700

    connected ranking update w/ Livedata.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/viewmodel/ScoresViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/viewmodel/ScoresViewModel.java
@@ -28,7 +28,7 @@ public class ScoresViewModel extends AndroidViewModel implements DefaultLifecycl
   private final MutableLiveData<Integer> poolSize;
   private final MutableLiveData<Boolean> sortedByTime;
   private final LiveData<List<GameSummary>> scoreboard;
-  private final MutableLiveData<List<RankedUser>> rankings;
+  private final LiveData<List<RankedUser>> rankings;
   private final MutableLiveData<Throwable> throwable;
   private final CompositeDisposable pending;
 
@@ -53,7 +53,7 @@ public class ScoresViewModel extends AndroidViewModel implements DefaultLifecycl
     scoreboard = Transformations.switchMap(trigger, (params) -> params.sortedByTime
         ? repository.getOrderedByTotalTime(params.poolSize, params.codeLength)
         : repository.getOrderedByGuessCount(params.poolSize, params.codeLength));
-    rankings = new MutableLiveData<>();
+    rankings = new RankingLiveData(trigger);
     throwable = new MutableLiveData<>();
     pending = new CompositeDisposable();
   }
@@ -100,18 +100,6 @@ public class ScoresViewModel extends AndroidViewModel implements DefaultLifecycl
     pending.clear();
   }
 
-  private void refreshRankings() {
-    pending.add(
-        repository
-            .getRankings(codeLength.getValue(), poolSize.getValue(),
-                sortedByTime.getValue() ? RankingOrder.TIME : RankingOrder.COUNT)
-            .subscribe(
-                rankings::postValue,
-                this::postThrowable
-            )
-    );
-  }
-
   private void postThrowable(Throwable throwable) {
     Log.e(getClass().getSimpleName(), throwable.getMessage(), throwable);
     this.throwable.postValue(throwable);
@@ -137,8 +125,7 @@ public class ScoresViewModel extends AndroidViewModel implements DefaultLifecycl
     public ScoreboardFilterLiveData(
         @NonNull LiveData<Integer> codeLength,
         @NonNull LiveData<Integer> poolSize,
-        @NonNull LiveData<Boolean> sortedByTime
-    ) {
+        @NonNull LiveData<Boolean> sortedByTime) {
       addSource(codeLength, (length) -> setValue(
           new ScoreboardParams(length, poolSize.getValue(), sortedByTime.getValue())));
       addSource(poolSize, (size) -> setValue(
@@ -147,6 +134,22 @@ public class ScoresViewModel extends AndroidViewModel implements DefaultLifecycl
           new ScoreboardParams(codeLength.getValue(), poolSize.getValue(), sorted)));
     }
 
+  }
+
+  private class RankingLiveData extends MediatorLiveData<List<RankedUser>> {
+
+    public RankingLiveData(@NonNull ScoreboardFilterLiveData liveParams) {
+      addSource(liveParams, (params) -> pending.add(
+          repository
+              .getRankings(params.codeLength, params.poolSize,
+                  params.sortedByTime ? RankingOrder.TIME : RankingOrder.COUNT)
+              .subscribe(
+                  this::postValue,
+                  ScoresViewModel.this::postThrowable
+              )
+          )
+      );
+    }
   }
 
 }
