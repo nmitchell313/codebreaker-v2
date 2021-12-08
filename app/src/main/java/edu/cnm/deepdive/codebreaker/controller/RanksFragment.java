c3b2781bb3e7dfa70e12ec5f8607154a67924e09
nmitchell313 <nmitchell11@cnm.edu>
2021-12-08 09:28:31 -0700

    completed player ranks screen.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/RanksFragment.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/RanksFragment.java
@@ -1,5 +1,6 @@
 package edu.cnm.deepdive.codebreaker.controller;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,8 +12,10 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import edu.cnm.deepdive.codebreaker.R;
 import edu.cnm.deepdive.codebreaker.adapter.GameSummaryAdapter;
+import edu.cnm.deepdive.codebreaker.adapter.RankedUserAdapter;
 import edu.cnm.deepdive.codebreaker.databinding.FragmentRanksBinding;
 import edu.cnm.deepdive.codebreaker.databinding.FragmentScoresBinding;
+import edu.cnm.deepdive.codebreaker.model.dto.RankedUser;
 import edu.cnm.deepdive.codebreaker.viewmodel.ScoresViewModel;
 
 public class RanksFragment extends Fragment {
@@ -23,6 +26,11 @@ public class RanksFragment extends Fragment {
   public View onCreateView(@NonNull LayoutInflater inflater,
       ViewGroup container, Bundle savedInstanceState) {
     binding = FragmentRanksBinding.inflate(inflater, container, false);
+    Resources resources = getResources();
+    binding.codeLengthDisplay.setText(
+        String.valueOf(resources.getInteger(R.integer.code_length_pref_default)));
+    binding.poolSizeDisplay.setText(
+        String.valueOf(resources.getInteger(R.integer.pool_size_pref_default)));
     binding.codeLength.setOnSeekBarChangeListener(
         (SimpleChangeListener) this::handleCodeLengthChange);
     binding.poolSize.setOnSeekBarChangeListener((SimpleChangeListener) this::handlePoolSizeChange);
@@ -38,8 +46,8 @@ public class RanksFragment extends Fragment {
     viewModel
         .getRankings()
         .observe(getViewLifecycleOwner(), (ranks) -> {
-//          GameSummaryAdapter adapter = new GameSummaryAdapter(getContext(), games);
-//          binding.games.setAdapter(adapter);
+          RankedUserAdapter adapter = new RankedUserAdapter(getContext(), ranks);
+          binding.ranks.setAdapter(adapter);
         });
     viewModel
         .getCodeLength()
